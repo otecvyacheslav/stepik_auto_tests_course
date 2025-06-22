46e7f037877fe81ea6228022b6a8401fc1de69ef
Vyacheslav_Voropaev <vsvoropaev@rambler.ru>
2025-06-22 11:30:35 +0300

    добавил сообщение в файл

--- lesson/stepik auto test/lesson2/lesson2.4_step8.py
+++ lesson/stepik auto test/lesson2/lesson2.4_step8.py
@@ -6,34 +6,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def math1(x): #указываем имя функции и ожидаемый элемент для функции
-    return str(math.log(abs(12 * math.sin(int(x))))) # здесь мы данный элемент кидаем в функцию
+def math1(x):  # указываем имя функции и ожидаемый элемент для функции
+    return str(
+        math.log(abs(12 * math.sin(int(x))))
+    )  # здесь мы данный элемент кидаем в функцию
 
 
-browser = webdriver.Chrome() # запуск хромдрайв
-browser.get("https://suninjuly.github.io/explicit_wait2.html") # переход по ссылке
+browser = webdriver.Chrome()  # запуск хромдрайв
+browser.get("https://suninjuly.github.io/explicit_wait2.html")  # переход по ссылке
 
 
-sell_price = WebDriverWait(browser, 15).until( #использую ожидание до 15 секунд
-    EC.text_to_be_present_in_element((By.ID, "price"), "$100") # пока не появится элемент $100 в необходимом ID
+sell_price = WebDriverWait(browser, 15).until(  # использую ожидание до 15 секунд
+    EC.text_to_be_present_in_element(
+        (By.ID, "price"), "$100"
+    )  # пока не появится элемент $100 в необходимом ID
 )
 
 
-book = browser.find_element(By.ID, "book") # простой поиск кнопки
-book.click() # и клик кнопки, там как выполнение идет последовательно, то оно произошло после найденного элемента вверху
+book = browser.find_element(By.ID, "book")  # простой поиск кнопки
+book.click()  # и клик кнопки, там как выполнение идет последовательно, то оно произошло после найденного элемента вверху
 
-x_find = browser.find_element(By.ID, "input_value") # поиск значения Х в тексте
-x = x_find.text # вытащили текст
-y = math1(x) # задали переменную для Х и выщвали функцию, чтобы ее посчитать
+x_find = browser.find_element(By.ID, "input_value")  # поиск значения Х в тексте
+x = x_find.text  # вытащили текст
+y = math1(x)  # задали переменную для Х и выщвали функцию, чтобы ее посчитать
 
-input1 = browser.find_element(By.ID, "answer") # нашли куда вводить 
-input1.send_keys(y) # ввели посчитанное
+input1 = browser.find_element(By.ID, "answer")  # нашли куда вводить
+input1.send_keys(y)  # ввели посчитанное
 
-enter = browser.find_element(By.ID, "solve") #нашли кнопку
-enter.click() # нажали
+enter = browser.find_element(By.ID, "solve")  # нашли кнопку
+enter.click()  # нажали
 
-time.sleep(10) # ждем 10 сек, чтобы скопировать полученный код
-browser.quit # закрыли браузер
+time.sleep(10)  # ждем 10 сек, чтобы скопировать полученный код
+browser.quit  # закрыли браузер
 
 # ниже поля из страницы, чтобы не искать на ней в моменте, а сразу выбрать из представленного
 # ++
@@ -43,3 +47,5 @@ browser.quit # закрыли браузер
 # input <input class="form-control" name="text" id="answer" type="text" required="">
 
 # enter <button id="solve" type="submit" class="btn btn-primary" disabled="disabled">Submit</button>
+
+# хочу добавить изменение в файл, чтобы оно появилось в гите
