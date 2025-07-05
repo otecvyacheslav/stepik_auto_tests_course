bbcdaff0c9ef1e3415d9810519fa2d57eed87253
Vyacheslav_Voropaev <vsvoropaev@rambler.ru>
2025-07-06 00:35:06 +0300

    подготовка перед юниттестом

---
+++ lesson/stepik auto test/lesson3/lesson3.2_step13.py
@@ -0,0 +1,5 @@
+import unittest
+
+class Test_step13(unittest.TestCase):
+    def testЗадание_уникальность_селекторов(self):
+        self.assertEqual("Congratulations! You have successfully registered!")
