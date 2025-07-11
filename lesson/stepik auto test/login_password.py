b4d5269974104c07cf238d951d82aaf9d29c88ed
Vyacheslav_Voropaev <vsvoropaev@rambler.ru>
2025-07-11 12:22:46 +0300

    добавлен инпут

--- lesson/stepik auto test/login_password.py
+++ lesson/stepik auto test/login_password.py
@@ -5,13 +5,13 @@ def login():
     browser = webdriver.Chrome()
     login = browser.find_element(By.CSS_SELECTOR, "[name=Login]")
     print("\nввод логина")
-    login.send_keys("support")
+    login.send_keys("надо логин")
 
 def password():
     browser = webdriver.Chrome()
     password = browser.find_element(By.CSS_SELECTOR, "[name=Password]")
     print("\nввод пароля")
-    password.send_keys("Solforb.2015")
+    password.send_keys("надо пароль")
     button = browser.find_element(By.CSS_SELECTOR, "button[type=submit]")
     button.click()
 
